0838160e3d7bf466c7c5f7ca0a6f1d99e0054264
Natália Arantes <nataliaarantes@Nats-Laptop.local>
2025-05-12 17:54:48 -0300

    primeiro commit

---
+++ mvcSofin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mvcSofin
+//
+//  Created by Natália Arantes on 12/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mvcSofin/mvcSofinApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvcSofinApp.swift
+//  mvcSofin
+//
+//  Created by Natália Arantes on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct mvcSofinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
